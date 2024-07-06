1846e7d92b6cf3d2b30c3ad8bdc9a2dcb952e2b6
Haseab <haseab.amin@gmail.com>
2024-07-05 23:22:27 -0700

    log credentials for debuggability

--- src/analyzer.py
+++ src/analyzer.py
@@ -15,6 +15,8 @@ class Analyzer:
     def __init__(self):
         load_dotenv()
         credentials = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
+        print("CREDENTIALS ARE")
+        print(credentials)
         self.credentials = self.load_credentials(credentials)
 
         self.unplanned = GoogleCalendar(
